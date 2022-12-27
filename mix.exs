334857915b648c500282e153c22de267afb3c04c
DJ Daugherty <ddaugher@gmail.com>
2022-12-27 09:17:30 -0500

    updating version to 1.12 (named bindings)

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TagsMultiTenant.Mixfile do
   def project do
     [app: :tags_multi_tenant,
      name: "TagsMultiTenant",
-     version: "0.1.11",
+     version: "0.1.12",
      elixir: "~> 1.10",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
