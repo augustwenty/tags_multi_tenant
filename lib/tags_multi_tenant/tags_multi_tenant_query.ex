79a8ebd4313dfaef19851bf0d6c99e66ffe959ad
DJ Daugherty <ddaugher@gmail.com>
2022-12-27 08:49:21 -0500

    updating to use named bindings

--- lib/tags_multi_tenant/tags_multi_tenant_query.ex
+++ lib/tags_multi_tenant/tags_multi_tenant_query.ex
@@ -14,16 +14,18 @@ defmodule TagsMultiTenant.TagsMultiTenantQuery do
   def search_tags(context, taggable_type, taggable_id \\ nil) do
     Tag
     |> join_taggings_from_tag(context, taggable_type, taggable_id)
-    |> distinct([t, tg], t.name)
-    |> order_by([t, tg], asc: tg.inserted_at)
-    |> select([t, tg], t.name)
+    |> join_tags
+    |> distinct([tags: tags], tags.name)
+    |> order_by([taggings: taggings], asc: taggings.inserted_at)
+    |> select([tags: tags], tags.name)
   end
 
   defp join_taggings_from_tag(query, context, taggable_type, nil) do
     query
-    |> join(:inner, [t], tg in Tagging,
+    |> join(:inner, [tag], tg in Tagging,
+      as: :taggings,
       on:
-        t.id == tg.tag_id and
+        tag.id == tg.tag_id and
           tg.taggable_type == ^taggable_type and
           tg.context == ^context
     )
@@ -31,9 +33,10 @@ defmodule TagsMultiTenant.TagsMultiTenantQuery do
 
   defp join_taggings_from_tag(query, context, taggable_type, taggable_id) do
     query
-    |> join(:inner, [t], tg in Tagging,
+    |> join(:inner, [tag], tg in Tagging,
+      as: :taggings,
       on:
-        t.id == tg.tag_id and
+        tag.id == tg.tag_id and
           tg.taggable_type == ^taggable_type and
           tg.context == ^context and
           tg.taggable_id == ^taggable_id
@@ -44,17 +47,20 @@ defmodule TagsMultiTenant.TagsMultiTenantQuery do
   Build the query to search tagged resources
   """
   def search_tagged_with(query, tags, context, taggable_type) do
+    IO.inspect("------------------------")
     tags_length = length(tags)
 
     query
     |> join_taggings_from_model(context, taggable_type)
+    |> IO.inspect()
     |> join_tags
-    |> where([m, tg, t], t.name in ^tags)
-    |> group_by([m, tg, t], m.id)
-    |> having([m, tg, t], count(tg.taggable_id) == ^tags_length)
-    |> order_by([m, t, tg], asc: m.inserted_at)
-
-    # |> select([m, tg, t], m)
+    |> IO.inspect()
+    |> where([tags: tags], tags.name in ^tags)
+    |> IO.inspect()
+    |> group_by([m], m.id)
+    |> having([taggings: taggings], count(taggings.taggable_id) == ^tags_length)
+    |> order_by([m], asc: m.inserted_at)
+    |> select([m], m)
   end
 
   @doc """
@@ -75,17 +81,17 @@ defmodule TagsMultiTenant.TagsMultiTenantQuery do
   def count_tagging_by_tag_id(tag_id) do
     Tagging
     |> where([t], t.tag_id == ^tag_id)
-    |> select([p], count(p.id))
+    |> select([m], count(m.id))
   end
 
   # Get ALL Tags related to context and taggable_type
   defp get_all_tags(tag_resource, taggable_type, context) do
     Tagging
     |> where(
-      [t],
-      t.tag_id == ^tag_resource.id and
-        t.taggable_type == ^taggable_type and
-        t.context == ^context
+      [tag],
+      tag.tag_id == ^tag_resource.id and
+        tag.taggable_type == ^taggable_type and
+        tag.context == ^context
     )
   end
 
@@ -93,21 +99,27 @@ defmodule TagsMultiTenant.TagsMultiTenantQuery do
   defp get_only_tags_related(tag_resource, taggable_type, taggable_id, context) do
     tag_resource
     |> get_all_tags(taggable_type, context)
-    |> where([t], t.taggable_id == ^taggable_id)
+    |> where([tag], tag.taggable_id == ^taggable_id)
   end
 
   defp join_taggings_from_model(query, context, taggable_type) do
-    query
-    |> join(:inner, [m], tg in Tagging,
-      on:
-        tg.taggable_type == ^taggable_type and
-          tg.context == ^context and
-          m.id == tg.taggable_id
-    )
+    if has_named_binding?(query, :taggings) do
+      query
+    else
+      query
+      |> join(:inner, [m], t in Tagging,
+        as: :taggings,
+        on: t.taggable_type == ^taggable_type and t.context == ^context and m.id == t.taggable_id
+      )
+    end
   end
 
   defp join_tags(query) do
-    query
-    |> join(:inner, [m, tg], t in Tag, on: t.id == tg.tag_id)
+    if has_named_binding?(query, :tags) do
+      query
+    else
+      query
+      |> join(:inner, [taggings: taggings], t in Tag, as: :tags, on: t.id == taggings.tag_id)
+    end
   end
 end
