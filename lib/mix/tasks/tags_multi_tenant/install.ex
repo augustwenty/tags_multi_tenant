57013b7bd918c9f7353b72611addb952400b9433
DJ Daugherty <ddaugher@gmail.com>
2023-05-25 22:02:14 -0400

    updating install.ex to include unique index creation

--- lib/mix/tasks/tags_multi_tenant/install.ex
+++ lib/mix/tasks/tags_multi_tenant/install.ex
@@ -6,13 +6,13 @@ defmodule Mix.Tasks.TagsMultiTenant.Install do
   import Mix.Tasks.TagsMultiTenant.Utils, only: [timestamp: 0]
 
   def run(_args) do
-    path = Path.relative_to("priv/repo/migrations", Mix.Project.app_path)
+    path = Path.relative_to("priv/repo/migrations", Mix.Project.app_path())
     tag_file = Path.join(path, "#{timestamp()}_create_tag.exs")
     tagging_file = Path.join(path, "#{timestamp()}_create_tagging.exs")
     taggable_id_type = Application.get_env(:tags_multi_tenant, :taggable_id, :integer)
-    create_directory path
+    create_directory(path)
 
-    create_file tag_file, """
+    create_file(tag_file, """
     defmodule Repo.Migrations.CreateTag do
       use Ecto.Migration
 
@@ -20,11 +20,13 @@ defmodule Mix.Tasks.TagsMultiTenant.Install do
         create table(:tags) do
           add :name, :string, null: false
         end
+
+        create_if_not_exists(unique_index(:tags, [:name], name: :tags_name_key))
       end
     end
-    """
+    """)
 
-    create_file tagging_file, """
+    create_file(tagging_file, """
     defmodule Repo.Migrations.CreateTagging do
       use Ecto.Migration
 
@@ -41,6 +43,6 @@ defmodule Mix.Tasks.TagsMultiTenant.Install do
         create index(:taggings, [:taggable_id, :taggable_type, :context])
       end
     end
-    """
+    """)
   end
 end
