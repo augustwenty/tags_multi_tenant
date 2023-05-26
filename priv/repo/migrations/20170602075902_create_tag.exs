cf6b76510761d08f89f919327e2065de41757232
DJ Daugherty <ddaugher@gmail.com>
2023-05-25 21:57:19 -0400

    pushing new version to support unique tag name

--- priv/repo/migrations/20170602075902_create_tag.exs
+++ priv/repo/migrations/20170602075902_create_tag.exs
@@ -3,7 +3,9 @@ defmodule TagsMultiTenant.Repo.Migrations.CreateTag do
 
   def change do
     create table(:tags) do
-      add :name, :string, null: false
+      add(:name, :string, null: false)
     end
+
+    create_if_not_exists(unique_index(:tags, [:name], name: :tags_name_key))
   end
 end
--- priv/repo/migrations/20170602081401_update_tag_unique_index.exs
+++
@@ -1,9 +0,0 @@
-defmodule TagsMultiTenant.Repo.Migrations.UpdateTagUniqueIndex do
-  use Ecto.Migration
-
-  def change do
-    drop_if_exists(unique_index(:tags, [:name], name: :tags_name_key))
-
-    create_if_not_exists(unique_index(:tags, [:name], name: :tags_name_key))
-  end
-end
